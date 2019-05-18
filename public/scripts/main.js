91398db688069aaf19ce93d20448939c9830646d
Christine Benedict <ccbenedict3@gmail.com>
2019-05-18 12:12:33 -0700

    urlParams with a form

---
+++ public/scripts/main.js
@@ -0,0 +1,20 @@
+// General functions
+
+function getURLparams(param) {
+	var pageUrl = window.location.search.substring(1);
+	var pageUrlVariables = pageUrl.split('&');
+
+	for(var i=0; i <pageUrlVariables.length; i++){
+		var parameterName = pageUrlVariables[i].split('=');
+		if(parameterName[0] == param) {
+			return parameterName[1];
+		}
+	}
+}
+
+var username = getURLparams('username');
+if('undefied' == typeof username || !username){
+	username = "Wookie"+Math.floor(Math.random()*10000);
+}
+
+$('#messages').append('<h4 class="name">'+username+'</h4>');
